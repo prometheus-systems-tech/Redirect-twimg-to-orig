8c83f34f51e793e1231f89127be38615b5f39619
prometheus-systems-tech <rwd.cameron@prometheus-systems.co.za>
2025-05-09 15:05:53 +0200

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Redirect twimg to :orig
+// @namespace    https://prometheus-systems.co.za/
+// @version      0.0.5
+// @description  Redirect Twitter image URLs to enforce name=orig for full-resolution images
+// @author       rwdcameron
+// @license      GPL-2.0-only
+// @supportURL   mailto://rwd.cameron@prometheus-systems.co.za?Subject=twimg-resizer
+// @match        *://pbs.twimg.com/media/*
+// @exclude      *://pbs.twimg.com/media/*?*name=orig*
+// @run-at       document-start
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/prometheus-systems-tech/Redirect-twimg-to-orig/refs/heads/main/redirector.js
+// @downloadURL  https://raw.githubusercontent.com/prometheus-systems-tech/Redirect-twimg-to-orig/refs/heads/main/redirector.js
+// ==/UserScript==
